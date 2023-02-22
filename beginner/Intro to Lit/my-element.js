5f4289189bd99c7e58abf415ef5e88ef8ff5b54c
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-22 03:50:41 -0600

    my-element.js

--- beginner/Intro to Lit/my-element.js
+++ beginner/Intro to Lit/my-element.js
@@ -1,11 +1,18 @@
 import {LitElement, html} from 'lit';
 
 export class MyElement extends LitElement {
-  // TODO: Add a reactive property
+  static properties = {
+    message: {},
+  };
+
+  constructor() {
+    super();
+    this.message = 'Hello again.';
+  }
 
   render() {
     return html`
-      <p>TODO: Add an expression</p>
+      <p>${this.message}</p>
     `;
   }
 }
