4c32188fba3b33ac24b17e79ed85667c8206a5a6
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-22 06:13:46 -0600

    word-viewer.js

--- beginner/Build a word viewer!/word-viewer.js
+++ beginner/Build a word viewer!/word-viewer.js
@@ -12,6 +12,9 @@ class WordViewer extends LitElement {
     this.words = 'initial value';
   }
 
+  // TODO: Define a `connectedCallback` which sets an interval.
+  // TODO: Define a `disconnectedCallback` which clears the interval.
+
   render() {
     const splitWords = this.words.split('.');
     const word = splitWords[this.idx % splitWords.length];
