31dd6b382c5a1decfd24afb1e25b9bb2550c2500
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-22 06:12:39 -0600

    word-viewer.js

--- beginner/Build a word viewer!/word-viewer.js
+++ beginner/Build a word viewer!/word-viewer.js
@@ -7,10 +7,13 @@ class WordViewer extends LitElement {
 
   constructor() {
     super();
+    // TODO: Add `idx` state
     this.words = 'initial value';
   }
 
   render() {
+    // TODO: Split the `words` by `'.'`, and from the resulting word array
+    // only show the word on index `this.idx`.
     return html`<pre>${this.words}</pre>`;
   }
 }
