39910a3061e8a0d388397339a24262c5c06f81ab
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-22 06:01:45 -0600

    my-element.js

--- beginner/Working with lists/my-element.js
+++ beginner/Working with lists/my-element.js
@@ -2,23 +2,39 @@ import {LitElement, html} from 'lit';
 
 class MyElement extends LitElement {
   static properties = {
-    names: {state: true},
+    friends: {state: true},
+    pets: {state: true},
+    includePets: {state: true},
   };
 
   constructor() {
     super();
-    this.names = ['Chandler', 'Phoebe', 'Joey', 'Monica', 'Rachel', 'Ross'];
+    this.friends = ['Harry', 'Ron', 'Hermione'];
+    this.pets = [
+      {name: 'Hedwig', species: 'Owl'},
+      {name: 'Scabbers', species: 'Rat'},
+      {name: 'Crookshanks', species: 'Cat'},
+    ];
+    this.includePets = true;
   }
 
   render() {
+    const listItems = [];
+    // TODO: populate templates with items to render.
+
     return html`
-      <p>A list of names that include the letter "e"</p>
+      <button @click=${() => this._togglePetVisibility()}>
+        ${this.includePets ? 'Hide' : 'Show'} pets
+      </button>
+      <p>My magical friends</p>
       <ul>
-      ${this.names
-        .filter((name) => name.match(/e/i))
-        .map((name) => html`<li>${name}</li>`)}
+        <!-- TODO: Render templates. -->
       </ul>
     `;
+  }
+
+  _togglePetVisibility() {
+    this.includePets = !this.includePets;
   }
 }
 customElements.define('my-element', MyElement);
