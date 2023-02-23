f6ca98440894a937b2493633b20034e1970c798e
Emiliano530 <emiliano.19070011@itsmotul.edu.mx>
2023-02-23 00:29:35 -0600

    repeat-pattern.js

--- intermediate/Repeat patterns with SVG/repeat-pattern.js
+++ intermediate/Repeat patterns with SVG/repeat-pattern.js
@@ -1,8 +1,30 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, svg} from 'lit';
+
+const createElement = (chars) => svg`
+  <text
+    dominant-baseline="hanging"
+    font-family="monospace"
+    font-size="24px">
+    ${chars}
+  </text>
+`;
 
 export class RepeatPattern extends LitElement {
+  static properties = {
+    chars: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.chars = 'lit';
+  }
+
   render() {
-    return html`<svg height="100%" width="100%"></svg>`;
+    return html`
+      <svg height="100%" width="100%">
+        ${createElement(this.chars)}
+      </svg>
+    `;
   }
 }
 customElements.define('repeat-pattern', RepeatPattern);
